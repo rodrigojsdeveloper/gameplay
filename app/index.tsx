4367218bd8a15604aeb4eee17b740e8e4d3ef1af
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-08-03 13:18:11 -0300

    feat: add images

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, SafeAreaView, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <View></View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
